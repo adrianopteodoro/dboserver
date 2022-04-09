d81ca57cdba435b4fe2e03e700f17d9e7c48e960
Adriano Teodoro <adrianopteodoro@gmail.com>
2022-04-08 22:33:49 -0300

    Update to net core 6.0

--- BaseLib/Properties/AssemblyInfo.cs
+++ BaseLib/Properties/AssemblyInfo.cs
@@ -1,8 +1,5 @@
 ﻿using System.Reflection;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
 [assembly: AssemblyTitle("Server System Base Library")]
 [assembly: AssemblyDescription("Default Server System Libraries")]
 [assembly: AssemblyConfiguration("")]
@@ -11,15 +8,4 @@
 [assembly: AssemblyCopyright("Copyright © 2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0")]
